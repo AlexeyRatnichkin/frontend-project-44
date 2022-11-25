0948c6b168bd3c3eaffc474e6f8590733937098c
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-11-25 16:41:05 +0500

    add bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,25 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');   
-let i = 0;
-while (i < 3) {
-  const quest = readlineSync.question('Question: ');
-  const questt = readlineSync.question('Your answer: ');
-  if (quest % 2 === 0 && questt === 'yes') {
-      console.log('Correct!');
-      i += 1;
-    } else if (quest % 2 !== 0 && questt === 'no') {
-      console.log('Correct!');
-      i += 1;  
-    } else {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log("Let's try again, Bill!");
-      break;
-  };
-  if (i === 3) {
-  console.log(`Congratulations, ${name}!`);
-  };  
-};
+import hello from '/home/alexey/frontend-project-44/src/cli.js';  
+
+hello();
+
+import even from '/home/alexey/frontend-project-44/src/games/even.js'; 
+
+even();
