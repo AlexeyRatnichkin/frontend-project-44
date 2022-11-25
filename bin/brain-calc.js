c9fa19ea849570c0e4607f3e20b4acfbe78435c9
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-11-25 16:46:09 +0500

    add bin/brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+
+import hello from '/home/alexey/frontend-project-44/src/cli.js';  
+
+hello();
+
+import calc from '/home/alexey/frontend-project-44/src/games/calc.js'
+
+calc()
+
