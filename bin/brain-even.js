19a7e7d3294a2a4a6defd0f53af5d674bf27f6af
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-11-28 23:12:02 +0500

    bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,5 @@
 import readlineSync from 'readline-sync';
 
-import hello from '/home/alexey/frontend-project-44/src/cli.js';  
-
-hello();
-
 import even from '/home/alexey/frontend-project-44/src/games/even.js'; 
 
 even();
