496496119676ce097c3139e7fd5268be976b562e
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-11-28 23:16:34 +0500

    src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const even = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no"')
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no" ')
   let i = 0;
   while (i < 3) {
     const quest = readlineSync.question('Question: ');
@@ -14,7 +17,7 @@ const even = () => {
         i += 1;  
       } else {
         console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log("Let's try again, Bill!");
+        console.log(`Let's try again, ${name}!`);
         break;
     };
     if (i === 3) {
