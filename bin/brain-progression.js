8becf8281e4f0b0d4f5ecb5509d1bb6dcfb2a28f
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-01 12:55:42 +0500

    add bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
-import progression from '/home/alexey/frontend-project-44/src/games/progression.js';
+import progression from '../src/games/progression.js';
 
 progression();
