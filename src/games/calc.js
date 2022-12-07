7b1c0f92810b5c4af71e58aefc18497bbde3f883
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-07 20:00:42 +0500

    add src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,41 +1,44 @@
-import readlineSync from 'readline-sync';
+import getRandomNumber from '../utils.js';
+import gameEngine from '../index.js';
 
-const calc = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('What is the result of the expression?');
-  let i = 0;
-  let c = 0;  
-  while (i < 3) {
-    const znak = ['+', '-', '*'];
-    const random = znak[Math.floor(Math.random() * znak.length)];
-    let a = Math.floor(Math.random() * 101);
-    let b = Math.floor(Math.random() * 101);
-    console.log('Question: ' + a + ' ' + random + ' ' + b);
-    const quest2 = readlineSync.question('Your answer: ');
-  
-    if (random === '+') {
-      c = a + b;
-    } else if (random === '-') {
-      c = a - b;
-    } else {
-      c = a * b;
-    }
+/* Цель игры.*/
+const description = 'What is the result of the expression?';
 
-    if (Number(c) === Number(quest2)) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`'${quest2}', is wrong answer ;(. Correct answer was, '${c}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-  
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    };  
+/* Массив с возможными операциями.*/
+const operations = ['+', '-', '*'];
+
+/* Получение случайной операции.*/
+function getRandomOperation(arrayWithOperations) {
+  const rand = Math.floor(Math.random() * arrayWithOperations.length);
+  return operations[rand];
+}
+
+/* Получение верного ответа.*/
+function calculate(firstNumber, secondNumber, operation) {
+  switch (operation) {
+    case '+':
+      return (firstNumber + secondNumber).toString();
+    case '-':
+      return (firstNumber - secondNumber).toString();
+    default:
+      return (firstNumber * secondNumber).toString();
   }
 }
 
-export default calc;
+/* Массив с вопросами и ответами.*/
+function getQuestAndAnswer() {
+  let firstNumber = 0;
+  let secondNumber = 0;
+  let operation = '';
+  firstNumber = getRandomNumber(0, 10);
+  secondNumber = getRandomNumber(0, 10);
+  operation = getRandomOperation(operations);
+  const question = `${firstNumber} ${operation} ${secondNumber} = ?`;
+  const correctAnswer = calculate(firstNumber, secondNumber, operation);
+
+  return [question, correctAnswer];
+}
+
+export default () => {
+  gameEngine(description, getQuestAndAnswer);
+};
