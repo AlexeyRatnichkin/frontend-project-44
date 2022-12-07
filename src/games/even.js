2c6f96c30fe07bed773dfb8ddd469febb59b6e4b
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-07 20:01:14 +0500

    add src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,30 +1,22 @@
-import readlineSync from 'readline-sync';
+import getRandomNumber from '../utils.js';
+import gameEngine from '../index.js';
 
-const even = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no" ')
-  let i = 0;
-  while (i < 3) {
-    let a = Math.abs(Math.floor(Math.random() * 101))
-    console.log('Question: ' + a);
-    const questt = readlineSync.question('Your answer: ');
-    if (Number(a) % 2 === 0 && questt === 'yes') {
-        console.log('Correct!');
-        i += 1;
-      } else if (Number(a) % 2 !== 0 && questt === 'no') {
-        console.log('Correct!');
-        i += 1;  
-      } else {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log(`Let's try again, ${name}!`);
-        break;
-    };
-    if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-    };  
-  };
+/* Цель игры.*/
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+/* Функция проверки на четность.*/
+function isEven(number) {
+  return number % 2 === 0;
 }
 
-export default even;
+/* Создаем массив вопросов и ответов.*/
+function getQuestAndAnswer() {
+  const question = getRandomNumber(0, 1000);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+}
+
+export default () => {
+  gameEngine(description, getQuestAndAnswer);
+};
