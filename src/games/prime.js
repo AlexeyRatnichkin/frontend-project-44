9cdb453a923eeef10dc27e827ed3daa90b1aa60e
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-07 20:02:30 +0500

    add src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,40 +1,27 @@
-import readlineSync from 'readline-sync';
+import getRandomNumber from '../utils.js';
+import gameEngine from '../index.js';
 
-const prime = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  let i = 0;
-  while (i < 3) {
-    let a = Math.abs(Math.floor(Math.random() * 101));
-    let j = 1;
-    let result = [];
-    console.log('Question: ' + a);
-    const quest2 = readlineSync.question('Your answer: ');
-    while (j < a + 1) {
-      if (Number.isInteger(a / j) === true) {
-        result.push(j);
-      }
-      j += 1;
-    } 
+/* Цель игры */
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".ind the greatest common divisor of given numbers.';
 
-    if (result.length === 2 && quest2 === 'yes') {
-      console.log('Correct!');
-      i += 1;   
-    } else if (result.length > 2 && quest2 === 'no') {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      console.log(`Let's try again, ${name}!`);
-      break;
+/* Проверка на простое число.*/
+function isTheNumberPrime(number) {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
-  
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }   
-  } 
-} 
+  }
+  return true;
+}
 
-export default prime; 
+/* Получаем массив вопросов и ответов.*/
+function getQuestAndAnswer() {
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = isTheNumberPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+}
+
+export default () => {
+  gameEngine(description, getQuestAndAnswer);
+}; 
