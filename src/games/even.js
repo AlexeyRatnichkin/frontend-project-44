e2a1ccfe6dcb461d5c05b3bd2977091b7db4b568
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-08 09:38:58 +0500

    add src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,15 @@
 import getRandomNumber from '../utils.js';
 import gameEngine from '../index.js';
 
-/* Цель игры.*/
+/* Цель игры. */
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-/* Функция проверки на четность.*/
+/* Функция проверки на четность. */
 function isEven(number) {
   return number % 2 === 0;
 }
 
-/* Создаем массив вопросов и ответов.*/
+/* Создаем массив вопросов и ответов. */
 function getQuestAndAnswer() {
   const question = getRandomNumber(0, 1000);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
