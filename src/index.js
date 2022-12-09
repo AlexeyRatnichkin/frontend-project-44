79e780d978a4951be873f15ec09cdb58bea0a248
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-09 19:10:53 +0500

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@
 
 import readlineSync from 'readline-sync';
 
-/* Необходимое количество раундов */
+// Количество раундов.
 export const countRounds = 3;
 
-export default (nameOfGame, getQuestAndAnswer) => {
+export default (taskOfGame, getQuestAndAnswer) => {
   console.log('Welcome, to the   Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(nameOfGame);
+  console.log(taskOfGame);
 
-  /* Логика всей игры. */
-  /* Счетчик верных ответов. */
-  let i = 0;
-  while (i < countRounds) {
+  // Логика вопросов, получения ответов и их сравнение.
+  // Счетчик верных ответов.
+  for (let i = 0; i < countRounds; i += 1) {
     const [question, correctAnswer] = getQuestAndAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      i += 1;
     } else {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
       return console.log(`Let's try again, ${name}!`);
