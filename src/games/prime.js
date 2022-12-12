6511dcc95cd8e0431c8ea84f174cb986bb781584
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-12 13:20:22 +0500

    add src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,9 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 /* Проверка на простое число. */
 function isTheNumberPrime(number) {
+  if (number =< 1) {
+    return false;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
