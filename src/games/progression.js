110807a48a45a67c2ed88364a2ec89603ad18a04
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-20 11:37:49 +0500

    add src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,12 +1,12 @@
 import randomNumber from '../utils.js';
-import gameEngine from '../index.js';
+import buildGame from '../index.js';
 
-// Задача игры.
+/* Задача игры. */
 const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-// Создание массива с прогрессией.
-function getProgression(firstNumber, step) {
+/* Создание массива с прогрессией. */
+const getProgression = (firstNumber, step) => {
   const progression = [firstNumber];
   for (let i = 1; i < progressionLength; i += 1) {
     progression.push(firstNumber + (i * step));
@@ -14,8 +14,8 @@ function getProgression(firstNumber, step) {
   return progression;
 }
 
-// Получаем ответы и вопросы.
-function getQuestAndAnswer() {
+/* Получаем ответы и вопросы. */
+const getQuestAndAnswer = () => {
   const startNum = randomNumber(0, 100);
   const step = randomNumber(2, 9);
   const progression = getProgression(startNum, step);
@@ -28,5 +28,5 @@ function getQuestAndAnswer() {
 }
 
 export default () => {
-  gameEngine(description, getQuestAndAnswer);
+  buildGame(description, getQuestAndAnswer);
 };
