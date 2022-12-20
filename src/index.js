81638f44c5ae43d6ecd56e0596080d41d4c95489
AlexeyRatnichkin <alexeyratnichkin@icloud.com>
2022-12-20 14:45:13 +0500

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -17,14 +17,13 @@ const buildGame = (taskOfGame, getQuestAndAnswer) => {
     const [question, correctAnswer] = getQuestAndAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer !== correctAnswer) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
       console.log(`Let's try again, ${name}!`);
       return;
+      }
+    console.log('Correct!');
     }
-  }
   console.log(`Congratulations, ${name}!`);
 };
 
